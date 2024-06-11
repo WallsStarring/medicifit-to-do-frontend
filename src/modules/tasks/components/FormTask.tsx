9032cc4024cd88889fc527f9d4caa062e6150dae
WallsStarring <carnada3000@gmail.com>
2024-06-11 14:33:23 -0500

    initial project

---
+++ src/modules/tasks/components/FormTask.tsx
@@ -0,0 +1,226 @@
+import { useEffect, useState } from "react";
+import { Input } from "@nextui-org/input";
+import { Spacer } from "@nextui-org/spacer";
+import { Button } from "@nextui-org/button";
+import { Checkbox } from "@nextui-org/checkbox";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "@nextui-org/modal";
+
+import { BASE_URL } from "@/config/api.ts";
+
+interface ITask {
+  id: number;
+  completed: boolean;
+  title: string;
+}
+
+export const FormTask = () => {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState<ITask[]>([]);
+  const [editIndex, setEditIndex] = useState<number | null>(null);
+  const [editedTask, setEditedTask] = useState<string>("");
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const addTask = async () => {
+    try {
+      if (!task) {
+        setError("Por favor, ingresa una tarea.");
+
+        return;
+      }
+
+      const response = await fetch(`${BASE_URL}/tasks/create`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title: task }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to add task");
+      }
+      await fetchTasks();
+      setTask("");
+    } catch (error) {
+      console.error("Error adding task:", error);
+    }
+  };
+
+  const fetchTasks = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/tasks`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch tasks");
+      }
+      const data = await response.json();
+
+      setTasks(data.data.rows);
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+    }
+  };
+
+  const toggleTaskCompletion = async (index: number) => {
+    const updatedTasks = [...tasks];
+
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    try {
+      const response = await fetch(
+        `${BASE_URL}/tasks/${updatedTasks[index].id}/update`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updatedTasks[index]),
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to toggle task completion");
+      }
+      setTasks(updatedTasks);
+    } catch (error) {
+      console.error("Error toggling task completion:", error);
+    }
+  };
+
+  const deleteTask = async (id: number) => {
+    try {
+      const response = await fetch(`${BASE_URL}/tasks/${id}/delete`, {
+        method: "DELETE",
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to delete task");
+      }
+      await fetchTasks();
+    } catch (error) {
+      console.error("Error deleting task:", error);
+    }
+  };
+
+  const startEditing = (index: number) => {
+    setEditIndex(index);
+    setEditedTask(tasks[index].title);
+  };
+
+  const cancelEditing = () => {
+    setEditIndex(null);
+    setEditedTask("");
+  };
+
+  const saveTask = async (index: number) => {
+    try {
+      const response = await fetch(
+        `${BASE_URL}/tasks/${tasks[index].id}/update`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ title: editedTask }),
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to update task");
+      }
+      await fetchTasks();
+      setEditIndex(null);
+      setEditedTask("");
+    } catch (error) {
+      console.error("Error updating task:", error);
+    }
+  };
+
+  return (
+    <>
+      <h1>To-Do List</h1>
+      <div className={"flex items-center mb-2"}>
+        <Input value={task} onChange={(e) => setTask(e.target.value)} />
+        <Spacer y={0.5} />
+        <Button onClick={addTask}>Add Task</Button>
+      </div>
+      <Spacer y={1} />
+      {tasks.map((task, index) => (
+        <div key={index} className={"flex items-center mb-2 gap-1"}>
+          <Checkbox
+            checked={task.completed}
+            className={"flex-none"}
+            defaultSelected={task.completed}
+            onChange={() => toggleTaskCompletion(index)}
+          />
+          <Spacer x={0.5} />
+          {editIndex === index ? (
+            <>
+              <Input
+                autoFocus
+                value={editedTask}
+                onChange={(e) => setEditedTask(e.target.value)}
+              />
+              <Spacer x={0.5} />
+              <Button onClick={() => saveTask(index)}>Save</Button>
+              <Button onClick={cancelEditing}>Cancel</Button>
+            </>
+          ) : (
+            <>
+              <h1
+                className={"grow w-full"}
+                style={{
+                  textDecoration: task.completed ? "line-through" : "none",
+                }}
+              >
+                {task.title}
+              </h1>
+              <Button
+                className="mx-auto flex-none"
+                onClick={() => startEditing(index)}
+              >
+                Edit
+              </Button>
+              <Button onClick={() => deleteTask(task.id)}>Delete</Button>
+            </>
+          )}
+        </div>
+      ))}
+      <Modal isOpen={!!error} onOpenChange={(isOpen) => setIsOpen(isOpen)}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader>Error</ModalHeader>
+              <ModalBody>{error}</ModalBody>
+              <ModalFooter>
+                <Button
+                  onClick={() => {
+                    onClose();
+                    setError("");
+                  }}
+                >
+                  Close
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,53 @@
+import { Link } from "@nextui-org/link";
+import { button as buttonStyles } from "@nextui-org/theme";
+import { Card, CardBody } from "@nextui-org/card";
+
+import { siteConfig } from "@/config/site";
+import { subtitle, title } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+import DefaultLayout from "@/layouts/default";
+import { FormTask } from "@/modules/tasks/components/FormTask.tsx";
+
+export default function IndexPage() {
+  return (
+    <DefaultLayout>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-lg text-center justify-center">
+          <h1 className={title()}>Medicifit&nbsp;</h1>
+          <h1 className={title({ color: "violet" })}>To-Do&nbsp;</h1>
+          <br />
+          <h1 className={subtitle()}>Prueba técnina</h1>
+        </div>
+        <div className="flex gap-3">
+          <Link
+            isExternal
+            className={buttonStyles({
+              color: "primary",
+              radius: "full",
+              variant: "shadow",
+            })}
+            href={siteConfig.links.docs}
+          >
+            Documentation
+          </Link>
+          <Link
+            isExternal
+            className={buttonStyles({ variant: "bordered", radius: "full" })}
+            href={siteConfig.links.github}
+          >
+            <GithubIcon size={20} />
+            GitHub
+          </Link>
+        </div>
+
+        <div className="mt-8">
+          <Card className="w-full">
+            <CardBody>
+              <FormTask />
+            </CardBody>
+          </Card>
+        </div>
+      </section>
+    </DefaultLayout>
+  );
+}
